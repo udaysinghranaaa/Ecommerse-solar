b247699091351d7000571159dcfb2141d628ae2f
udaysinghranaaa <udaysinghranaaa@gmail.com>
2026-04-10 23:13:47 +0530

    added admin-check route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { createAdmin, login } = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
 router.post("/create-admin", createAdmin);
 router.post("/login", login);
+
+// 🔐 protected route
+router.get("/admin-check", auth, (req, res) => {
+  res.json({ success: true });
+});
 
 module.exports = router;
