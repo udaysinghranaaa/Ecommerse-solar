cd377872ef89cd58d3d4ac00ba4a4f20518571c0
uday <udaysinghranaaa@gmail.com>
2026-05-18 13:18:48 +0530

    fixed admin collection

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 
-const adminSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
 
-// ✅ correct model name
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: "admins",
+  }
+);
+
 module.exports =
   mongoose.models.Admin ||
   mongoose.model("Admin", adminSchema);
