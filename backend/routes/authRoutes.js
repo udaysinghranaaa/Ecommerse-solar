6eabf571a49b9c5aca33240bde0575686f70a1a4
uday <udaysinghranaaa@gmail.com>
2026-05-18 13:30:10 +0530

    removed temp admin route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const { createAdmin, login } = require("../controllers/authController");
+
 const auth = require("../middleware/authMiddleware");
+
+// create admin
 router.post("/create-admin", createAdmin);
+
+// login
 router.post("/login", login);
 
-// 🔐 protected route
+// protected route
 router.get("/admin-check", auth, (req, res) => {
-  res.json({ success: true });
+  res.json({
+    success: true,
+  });
 });
 
 module.exports = router;
